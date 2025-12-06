1d6a4968324b7813b9b20fe23001d3a4e8a0a8a1
Fearless0906 <jasonpiit9@gmail.com>
2025-12-06 09:15:35 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import obfuscator from 'rollup-plugin-obfuscator'
 
 export default defineConfig({
   plugins: [
@@ -12,7 +13,11 @@ export default defineConfig({
         entry: 'electron/main.js'
       }
     ]),
-    renderer()
+    renderer(),
+    obfuscator({
+      compact: true,
+      controlFlowFlattening: true
+    })
   ],
   server: {
     hmr: true,
